28224367b4faa26649b544ca9986d3e7aa9bc7c4
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2024-11-20 11:15:24 +0200

    прпр

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
-  base: "/notes/", // Замініть 'my-react-app' на назву вашого репозиторію, якщо потрібно
+  base: "/notes/", // Базовий шлях для вашого проєкту
   plugins: [react()],
   build: {
-    outDir: "dist",
+    outDir: "dist", // Директорія для зібраного проєкту
   },
   resolve: {
-    alias: {
-   },
+    alias: {}, // Залиште порожнім або повністю видаліть `resolve`, якщо аліаси не потрібні
   },
 });
